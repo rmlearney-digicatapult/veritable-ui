ad5a87d6636c3dc79958fa9aa9f5a009c2eabd5c
rmlearney-digicatapult <robert.learney@digicatapult.org.uk>
2025-08-23 20:49:15 +0100

    format timestamps

--- src/views/queries/queryResponse.tsx
+++ src/views/queries/queryResponse.tsx
@@ -420,7 +420,21 @@ export default class QueryResponseTemplates {
             <tr>
               <td>Date Certified:</td>
               <td class="query-results-left-padding-table">
-                <time>{Html.escapeHtml(new Date(query.updated_at))}</time>
+                <time>
+                  {Html.escapeHtml(
+                    `${new Date(query.updated_at)
+                      .toLocaleString('en-GB', {
+                        timeZone: 'Europe/London',
+                        year: 'numeric',
+                        month: '2-digit',
+                        day: '2-digit',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        hour12: false,
+                      })
+                      .replace(',', ' -')}`
+                  )}
+                </time>
               </td>
             </tr>
             <tr>
@@ -522,7 +536,21 @@ export default class QueryResponseTemplates {
             <tr>
               <td>Timestamp:</td>
               <td class="query-results-left-padding-table">
-                <time>{Html.escapeHtml(new Date(query.updated_at))}</time>
+                <time>
+                  {Html.escapeHtml(
+                    `${new Date(query.updated_at)
+                      .toLocaleString('en-GB', {
+                        timeZone: 'Europe/London',
+                        year: 'numeric',
+                        month: '2-digit',
+                        day: '2-digit',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        hour12: false,
+                      })
+                      .replace(',', ' -')}`
+                  )}
+                </time>
               </td>
             </tr>
             <this.bavResponseRow heading={'Country'} value={isoCountries.getName(responseData.countryCode, 'en')} />
